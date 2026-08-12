caf4b2ed1d7016f3aa323aa66151bf9763486925
02Kamogelo <makhayiamogelang@gmail.com>
2026-08-12 17:57:30 +0200

    Added Morse code translator program

---
+++ morse.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <map>
+#include <string>
+#include <cctype>
+using namespace std;
+
+int main() {
+    // Morse code dictionary
+    map<char, string> morse = {
+        {'A', ".-"},   {'B', "-..."}, {'C', "-.-."}, {'D', "-.."},
+        {'E', "."},    {'F', "..-."}, {'G', "--."},  {'H', "...."},
+        {'I', ".."},   {'J', ".---"}, {'K', "-.-"},  {'L', ".-.."},
+        {'M', "--"},   {'N', "-."},   {'O', "---"},  {'P', ".--."},
+        {'Q', "--.-"}, {'R', ".-."},  {'S', "..."},  {'T', "-"},
+        {'U', "..-"},  {'V', "...-"}, {'W', ".--"},  {'X', "-..-"},
+        {'Y', "-.--"}, {'Z', "--.."}
+    };
+
+    string message;
+    cout << "Enter a message: ";
+    getline(cin, message);
+
+    string fullMorse = "";
+
+    for (char c : message) {
+        if (isalpha(c)) {
+            char upper = toupper(c);
+            cout << upper << ": " << morse[upper] << endl;
+            fullMorse += morse[upper] + "   "; // 3 spaces between letters
+        } else if (c == ' ') {
+            fullMorse += "       "; // 7 spaces between words
+        }
+    }
+
+    cout << "\nFull Morse code with spaces:\n" << fullMorse << endl;
+
+    return 0;
+}
